2bdd5df55943082487742af6d64949588dc3df6f
MANIKJATANA <97248706+MANIKJATANA@users.noreply.github.com>
2025-10-27 00:02:19 +0530

    Add controller for membership part (#1)

---
+++ src/main/java/com/jatana/gymmembershipmanagemt/service/MembershipService.java
@@ -0,0 +1,17 @@
+package com.jatana.gymmembershipmanagemt.service;
+
+import com.jatana.gymmembershipmanagemt.model.dto.request.MembershipRequest;
+import com.jatana.gymmembershipmanagemt.model.dto.response.MembershipResponse;
+import com.jatana.gymmembershipmanagemt.model.dto.response.MembershipResponseWithPayment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MembershipService {
+    public MembershipResponse addMembership(String memberId, MembershipRequest membershipRequest) {
+        return null;
+    }
+
+    public MembershipResponseWithPayment getMembership(String memberId, String membershipId) {
+        return null;
+    }
+}
