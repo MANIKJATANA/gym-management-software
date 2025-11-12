71e42783dd6c88fe9079419bedbad0bf83927f87
MANIKJATANA <97248706+MANIKJATANA@users.noreply.github.com>
2025-11-12 13:17:43 +0530

    Feat/add doc (#14)
    * add indexing fix columns names,
    
    * add docker configuration
    
    * add documentation using swagger
    
    * add doc for project
    
    * allow all hosts

--- src/main/java/com/jatana/gymmembershipmanagemt/controller/TestController.java
+++ src/main/java/com/jatana/gymmembershipmanagemt/controller/TestController.java
@@ -2,8 +2,10 @@ package com.jatana.gymmembershipmanagemt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin
 public class TestController {
     @GetMapping("/status")
     public String status() {
